d32b0f20cf776865f95a950a407c4185b1869826
Satyam Sangeet <157357267+satyamsangeet@users.noreply.github.com>
2025-04-12 13:42:53 +1000

    Update model.m

--- Final_fitting/Unperturbed_fitting/Global_fit/model.m
+++ Final_fitting/Unperturbed_fitting/Global_fit/model.m
@@ -10,10 +10,10 @@ function dydt_n = model(t, y, params)
     sigma_A = params(9);
     r_p = params(10);
 
-    % Switch for daily cycle
+    % Switch
     sw_cycle = (mod(t, 24) >= 8 && mod(t, 24) < 24);
     
-    % ODE system
+    % ODE
     dydt_n = zeros(3, 1);
     dydt_n(1) = A * sw_cycle + sigma_A * A * (1 - sw_cycle) - (r_bp * sw_cycle + sigma_bp * r_bp * (1 - sw_cycle) + r_bc * sw_cycle + sigma_bc * r_bc * (1 - sw_cycle)) * y(1);
     dydt_n(2) = (r_bc * sw_cycle + sigma_bc * r_bc * (1 - sw_cycle)) * y(1) - (r_cp * sw_cycle + sigma_cp * r_cp * (1 - sw_cycle)) * y(2);
