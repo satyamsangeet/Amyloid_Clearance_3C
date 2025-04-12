0dafcce676494446cf4c89a605a9b0950efa17be
Satyam Sangeet <157357267+satyamsangeet@users.noreply.github.com>
2025-04-12 13:42:19 +1000

    Update main.m

--- Final_fitting/Unperturbed_fitting/Global_fit/main.m
+++ Final_fitting/Unperturbed_fitting/Global_fit/main.m
@@ -1,16 +1,14 @@
-% main.m
 clear;
 clc;
 
 rng(42);
 
-% Set up global variable
 global optimization_history;
 
-% Load data
+% Loading data
 [data, time] = load_data();
 
-% Optimization settings
+% Optimization requirements
 num_starts = 20;
 initial_guess = [1.5, 0.3, 0.5, 2.5, 3, 3, 2, 11, 0.8, 0.28];
 lb = [0.75, 0.01, 0.001, 1, 1, 1, 1, 9, 0.7, 0.23];
@@ -27,7 +25,7 @@ options = optimoptions('fmincon', ...
     'StepTolerance', 1e-6, ...
     'OutputFcn', @output_function);
 
-% Create optimization problem
+% Creating optimization problem
 problem = createOptimProblem('fmincon', ...
     'objective', @(params) objective_function(params, data.csf_conc_exp1, data.csf_conc_exp2, data.csf_conc_exp3, data.plasma_conc_exp1), ...
     'x0', initial_guess, ...
@@ -35,16 +33,15 @@ problem = createOptimProblem('fmincon', ...
     'ub', ub, ...
     'options', options);
 
-% Generate start points
+% Generating the custom start points
 customStartPoints = generate_start_points(initial_guess, lb, ub, num_starts);
 
-% Run optimization and save results
+% Run optim, save
 [solutions, best_params, best_fval, all_trajectories] = run_optimization(problem, customStartPoints, num_starts, data);
 
-% Visualize results
 plot_parameter_space(solutions, num_starts);
 
-% Print final results
+% Print
 fprintf('\nBest solution found:\n');
 fprintf('Parameters: r_bc=%.4f, r_bp=%.4f, r_cp=%.4f, sigma_bc=%.4f, sigma_bp=%.4f, sigma_cp=%.4f, sigma_p=%.4f, A=%.4f, sigma_A=%.4f, r_p=%.4f\n', ...
     best_params(1), best_params(2), best_params(3), best_params(4), best_params(5), best_params(6), best_params(7), best_params(8), best_params(9), best_params(10));
