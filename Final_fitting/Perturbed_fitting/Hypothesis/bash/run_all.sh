0994bef8d37890bd541fb13110fcf9f255b221f0
Satyam Sangeet <157357267+satyamsangeet@users.noreply.github.com>
2025-05-14 10:46:23 +1000

    Update run_all.sh

--- Final_fitting/Perturbed_fitting/Hypothesis/bash/run_all.sh
+++ Final_fitting/Perturbed_fitting/Hypothesis/bash/run_all.sh
@@ -3,13 +3,13 @@
 # List of hypothesis folders
 folders=("hypothesis1" "hypothesis2" "hypothesis3" "hypothesis4" "hypothesis5")
 
-# Loop through folders and run the existing bash script in each
+# Loop
 for folder in "${folders[@]}"; do
     echo "============================================"
     echo "Running MATLAB jobs in $folder..."
     (
         cd "$folder" || exit
-        chmod +x *.sh  # Ensure the script is executable
+        chmod +x *.sh  # executable
         ./$(basename *.sh)  # Run the only .sh script in the folder
     )
     echo "Finished $folder"
