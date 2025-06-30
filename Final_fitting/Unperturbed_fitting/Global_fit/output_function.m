ff45588d459260fde993e63618d4691faae04b87
Satyam Sangeet <157357267+satyamsangeet@users.noreply.github.com>
2025-06-30 11:23:29 +1000

    Delete Final_fitting directory

--- Final_fitting/Unperturbed_fitting/Global_fit/output_function.m
+++
@@ -1,8 +0,0 @@
-function stop = output_function(x, optimValues, state)
-    global optimization_history;
-    stop = false;
-    
-    if strcmp(state, 'iter')
-        optimization_history = [optimization_history; x];
-    end
-end
