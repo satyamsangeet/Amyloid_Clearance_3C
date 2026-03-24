a61d06a706956504053afc8a6f00e2611b5e87aa
Satyam Sangeet <157357267+satyamsangeet@users.noreply.github.com>
2026-03-24 19:24:48 +1100

    Add files via upload

---
+++ Normal_sleep_wake_fitting/Individual_fits/Blattner/euler.m
@@ -0,0 +1,17 @@
+function [t, w] = euler(F, endpoints, initial_conditions, ts)
+    if length(endpoints) == 2
+        h = ts;
+        total_time = endpoints(2) - endpoints(1);
+        num_steps = floor(total_time / h);
+        t = linspace(endpoints(1), endpoints(2), num_steps + 1);
+    else
+        h = endpoints(2) - endpoints(1);
+        t = endpoints;
+    end
+    w = zeros(num_steps+1, length(initial_conditions));
+    w(1,:) = initial_conditions;
+    for k = 1:num_steps
+        w(k+1,:) = w(k,:) + F(t(k), w(k,:))' * h;
+    end
+    t = t(:);
+end
